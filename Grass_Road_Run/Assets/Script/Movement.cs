de582f55bdd21e1d53de94b9a80d5ab20c221902
Md Sadiqul Islam Khan <46827272+sadik491@users.noreply.github.com>
2020-02-16 10:14:19 +0600

    add animation & obstical

--- Grass_Road_Run/Assets/Script/Movement.cs
+++ Grass_Road_Run/Assets/Script/Movement.cs
@@ -3,7 +3,9 @@
 public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
+    public Animator anim;
     public float speed;
+    float move = 1;
     
 
     void Start()
@@ -14,26 +16,34 @@ public class Movement : MonoBehaviour
     
     void FixedUpdate()
     {
+        ControlPlayer();
+
+    }
+
+    private void ControlPlayer()
+    {
         if (Input.GetKey("w"))
         {
-            rb.velocity = new Vector3(0, 0, speed);
-        }
-        else if (Input.GetKey("s"))
-        {
-            rb.MovePosition(new Vector3(-1, 0, 0));
-            //rb.velocity = new Vector3(0, speed, speed);
+            anim.SetTrigger("Run");
+            rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
         }
         else if (Input.GetKey("a"))
         {
-            rb.MovePosition(new Vector3(transform.position.x -1,transform.position.y, transform.position.z));
-            rb.velocity = new Vector3(0, 0, speed);
+            //rb.MovePosition(new Vector3(transform.position.x - 1, transform.position.y, transform.position.z));
+            //rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
+            rb.AddForce(-speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         else if (Input.GetKey("d"))
         {
-            rb.MovePosition(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z));
-            rb.velocity = new Vector3(0, 0, speed);
-            //rb.velocity = new Vector3(speed, 0, speed);
+            //rb.MovePosition(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z));
+            //rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
+            rb.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
         }
 
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
     }
 }
