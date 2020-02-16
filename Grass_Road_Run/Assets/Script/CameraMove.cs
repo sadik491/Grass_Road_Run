a1da50de9303c3e82fe8953e08515de64c187389
Md Sadiqul Islam Khan <46827272+sadik491@users.noreply.github.com>
2020-02-16 08:45:19 +0600

    Project File commited

---
+++ Grass_Road_Run/Assets/Script/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform Player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = Player.transform.position - transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Player.position - offset;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Grass_Road_Run/Assets/Script/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    
+
+    void Start()
+    {
+       
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb.MovePosition(new Vector3(-1, 0, 0));
+            //rb.velocity = new Vector3(0, speed, speed);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.MovePosition(new Vector3(transform.position.x -1,transform.position.y, transform.position.z));
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.MovePosition(new Vector3(transform.position.x + 1, transform.position.y, transform.position.z));
+            rb.velocity = new Vector3(0, 0, speed);
+            //rb.velocity = new Vector3(speed, 0, speed);
+        }
+
+    }
+}
